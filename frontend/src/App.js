fa247afd2468d844e7264ca0921280e2786af317
= <josephshaw1563@gmail.com>
2025-09-26 00:02:52 +0100

    Load existing cases on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 
 function App() {
+  const [cases, setCases] = useState([]);
+
+  useEffect(() => {
+    // TODO: Replace with environement variables to allow seamless transition to prod
+    fetch("http://127.0.0.1:8000/api/get_cases")
+      .then((res) => res.json())
+      .then((data) => setCases(data));
+  }, []);
+
   return (
-    <div className="App">
-        <p> Hello world </p>
-    
+    <div>
+      <h1>Case Manager</h1>
+      {cases.length === 0 ? (
+        <p>No cases found.</p>
+      ) : (
+        <ul>
+          {cases.map((c) => (
+            <li key={c.id} style={{ marginBottom: "10px" }}>
+              <b>{c.title}</b>, status:  {c.status}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
